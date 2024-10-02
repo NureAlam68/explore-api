d72c51db7bfe57ee2305116695ab75bd282031fb
NureAlam68 <nurealam151068@gmail.com>
2024-10-02 08:25:59 +0600

    get post patch delete

--- js/index.js
+++ js/index.js
@@ -1,21 +1,54 @@
-
-
 function loadUsers2() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(data => displayUsers2(data))
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => displayUsers2(data));
 }
 
-
 function displayUsers2(data) {
-    const ul = document.getElementById('list-container')
-    for(let user of data) {
-        // console.log(user);
-        console.log(user.name);
-        // console.log(user.email);
-        // console.log(user.username);
-        const li = document.createElement('li');
-        li.innerText = user.name;
-        ul.appendChild(li);
-    }
+  const ul = document.getElementById("list-container");
+  for (let user of data) {
+    // console.log(user);
+    console.log(user.name);
+    // console.log(user.email);
+    // console.log(user.username);
+    const li = document.createElement("li");
+    li.innerText = user.name;
+    ul.appendChild(li);
+  }
+}
+
+function deleteAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+}
+
+function patchAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function createAPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 }
